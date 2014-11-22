3aae22aa0977a12e21195dda2bbab5fd60feeb2a
Andrew Shepard <shep.andy@gmail.com>
2014-11-22 15:37:13 -0800

    Add OS X target

---
+++ HNReader OSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HNReader OSX
+//
+//  Created by Andrew Shepard on 11/22/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HNReader OSX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HNReader OSX
+//
+//  Created by Andrew Shepard on 11/22/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
